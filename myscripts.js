824aa7619db4e2506b9a820223099639326826e3
Samuel123098 <67918846+Samuel123098@users.noreply.github.com>
2024-12-05 12:59:27 +0000

    ITS WORKING

--- myscripts.js
+++ myscripts.js
@@ -1,6 +1,11 @@
-var display 
-display = document.getElementById("display")
-function skibidi(input){
+let display = document.getElementById("display")
+function DisplayToScreen(input){
  display.value = input
+}
 
+let image = document.getElementById("image")
+function party(){
+    image.style.animation = 'fade 5s ease-in-out';
+    image.style.opacity = 1;
+    
 }
