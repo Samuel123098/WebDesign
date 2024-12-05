86281ecc2375119f60ba2ff37c25aa1152556eb4
Samuel123098 <67918846+Samuel123098@users.noreply.github.com>
2024-12-05 13:41:01 +0000

    Finished

--- myscripts.js
+++ myscripts.js
@@ -1,11 +1,13 @@
 let display = document.getElementById("display")
 function DisplayToScreen(input){
  display.value = input
+ display.style.textAlign = 'center'
 }
 
 let image = document.getElementById("image")
 function party(){
-    image.style.animation = 'fade 5s ease-in-out';
+    image.style.animation = 'fade 20s ease-in';
     image.style.opacity = 1;
+    image.style.animationFillMode = 'forwards';
     
 }
